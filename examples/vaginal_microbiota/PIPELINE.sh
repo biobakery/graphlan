6e44e391aecfb92fe895d7283ad442d6cda12970
Francesco Asnicar <5664022+fasnicar@users.noreply.github.com>
2015-05-20 15:50:15 +0200

    All examples tested and fixed

--- examples/vaginal_microbiota/PIPELINE.sh
+++ examples/vaginal_microbiota/PIPELINE.sh
@@ -7,7 +7,7 @@ rm -f annot.txt tree.txt outtree.txt outimg.png outimg.svg
 # convert it!
 echo "Converting to GraPhlAn"
 export2graphlan.py -i stm_otu_table_with_taxonomy.biom -a annot.txt -t tree.txt --discard_otus --most_abundant 50 \
---annotations 2,3,4,5,6 --external_annotations 7 --title "STM" --internal_levels --max_clade_size 300 --ftop 50
+--annotations 2,3,4,5,6 --external_annotations 7 --title "STM" --internal_levels --max_clade_size 300
 
 echo "Running GraPhlAn"
 graphlan_annotate.py --annot annot.txt tree.txt outtree.txt # attach annotation to the tree
