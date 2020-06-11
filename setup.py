f3313cd793c89c238d525efe25deeaded59fd9c4
Francesco Beghini <francesco.beghini@unitn.it>
2020-06-11 09:34:05 +0000

    Added manifest file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 install_requires = ["biopython==1.76", "matplotlib>=1.1", "scipy"]
 setuptools.setup(
     name='graphlan',
-    version='1.1.3',
+    version='1.1.3.1',
     author='Francesco Asnicar',
     author_email='f.asnicar@unitn.it',
     url='http://github.com/biobakery/graphlan',
@@ -20,7 +20,7 @@ setuptools.setup(
     #                                        "graphlan_annotate.py = graphlan_annotate:main"
     #                                      ] },
     long_description_content_type='text/markdown',
-    long_description=open('readme.txt').read(),
+    long_description=open('readme.md').read(),
     description='GraPhlAn is a software tool for producing high-quality circular representations of taxonomic and phylogenetic trees. GraPhlAn focuses on concise, integrative, informative, and publication-ready representations of phylogenetically- and taxonomically-driven investigation.',
     install_requires=install_requires
 )
